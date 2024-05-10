220655b8e1e36fdf9a3a2b85a761d7e3a07cd3df
RzR <108324929+I-RzR-I@users.noreply.github.com>
2024-05-10 18:52:46 +0300

    Upgrade libs version (cve fix)

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,4 +1,8 @@
-### **1.0.1.8361** 
+### **v1.0.2.6703** 
+-> Update lib version for 'Npgsql', with `CVE-2024-32655` fix;<br />
+-> Update some libs version related to previous update;<br />
+
+### **v1.0.1.8361** 
 -> Change property mapper (map by property/column name);<br />
 -> Add EF DbContextExtension (that load neccessary information);<br />
 -> Add tests for simple select query and adjust mapper.<br />
---
+++ src/tests/DbObjectExecutorWithDINet6Test/Tests/SpNewTableIdTests.cs
@@ -0,0 +1,49 @@
+﻿using DbObjectExecutor.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedDbObjectExecutorInitInfo.DataBaseTool;
+using System.Data.SqlClient;
+
+namespace DbObjectExecutorWithDINet6Test.Tests
+{
+    [TestClass]
+    public class SpNewTableIdTests : InitInfo
+    {
+        [TestMethod]
+        public void SpNewTableId_Own_Transaction_Success_Test()
+        {
+            var objConn = new SqlConnection(DataBaseHelper.ConnectionStringDefaultMsSql);
+            
+            _dbObjectBuilder.SetInitInfo(DataBaseObjectNames.spNewTableId, objConn, DbExecutorType.Procedure).UseTransaction();
+            _dbObjectBuilder.SetIn("TableName", "TblX");
+            _dbObjectBuilder.SetOut("NextId", out var outNextId, -1);
+
+            _dbObjectBuilder.ExecuteNonQuery();
+
+            _dbObjectBuilder.CommitTransaction()
+                .Dispose();
+
+            Assert.IsNotNull(outNextId.Value);
+            Assert.IsTrue(outNextId.Value > -1);
+        }
+
+        [TestMethod]
+        public void SpNewTableId_Separate_Transaction_Success_Test()
+        {
+            var objConn = new SqlConnection(DataBaseHelper.ConnectionStringDefaultMsSql);
+            objConn.Open();
+            var trans = objConn.BeginTransaction();
+
+            _dbObjectBuilder.SetInitInfo(DataBaseObjectNames.spNewTableId, objConn, DbExecutorType.Procedure).UseTransaction(trans);
+            _dbObjectBuilder.SetIn("TableName", "TblX");
+            _dbObjectBuilder.SetOut("NextId", out var outNextId, 0);
+
+            _dbObjectBuilder.ExecuteNonQuery();
+
+            _dbObjectBuilder.CommitTransaction()
+                .Dispose();
+
+            Assert.IsNotNull(outNextId.Value);
+            Assert.IsTrue(outNextId.Value > -1);
+        }
+    }
+}
